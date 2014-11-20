11058c154fa3545843fe5a0f793fd712e10055f1
Wei-liang Huang <William@Wei-liangs-MacBook-Air.local>
2014-11-19 22:46:37 -0800

    added comments for clarifification

--- routes/google_inject_handler.js
+++ routes/google_inject_handler.js
@@ -26,21 +26,33 @@ var googleInjHandler = function(req, res) {
   request
   .get(url + 'key=' + apiKey + '&' + req.params.search)
   .end(function(req, gData) {
+    //grab data from google
     var parsedData = JSON.parse(gData.text);
+    //copy results from google
     var tempResults = parsedData.results;
+    //delete it to reinsert it later
     delete parsedData.results;
+    //map each object from tempresults array
     var mergedResults = _.map(tempResults, function(object) {
+      //create the right placeID key to search for in the db
       var key = {
         placeID: object.place_id
       };
+      console.log(key);
+      //retreve the document
       var query = Results.findOne(key);
       query.exec(function(err, results) {
         if (err) {
           return console.log('no data');
         }
+        //execute createTestResult() that will create an object
+        //speedTestResult with DL, UL, Ping, Parking
+        //insert the result into the object
         object.speedTestResults = results.createTestResult();
       });
+      //this mocktestresult object will be deleted, used for testing.
       object.mockTestResults = {leeroy:'jenkins'};
+      //return the modified element object
       return object;
     });
     parsedData.results = mergedResults;
