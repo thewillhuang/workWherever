adc2624ded4933e57a0574e7605c1e474a8e266a
Wei-liang Huang <will.h86@gmail.com>
2014-11-20 12:27:28 -0800

    fix typos

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var bodyparser = require('body-parser');
 var uriUtil = require('mongodb-uri');
 var options = { server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } },
                 replset: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } } };
-var mongodbUri = process.env.MONGOLAB_UR;
+var mongodbUri = process.env.MONGOLAB_URI;
 var mongooseUri = uriUtil.formatMongoose(mongodbUri);
 
 mongoose.connect(mongooseUri || 'mongodb://localhost/test', options);
