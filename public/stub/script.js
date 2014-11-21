b16f44c1d3f368ef0c3f78ed8593bb5bb98e5f7a
Hiroshi Nagata <hirosn@live.com>
2014-11-21 08:44:46 -0800

    Add search by name.

--- public/stub/script.js
+++ public/stub/script.js
@@ -1,14 +1,31 @@
 $(function() {
-  navigator.geolocation.getCurrentPosition(function(position) {
-    var location = position.coords.latitude + ',' + position.coords.longitude;
+  var getLocation = function() {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var location = position.coords.latitude + ',' + position.coords.longitude;
 
-    $('#current').text(location).attr('href',
-      'https://work-wherever.herokuapp.com/google/inj/location=' +
-      location + '&radius=500&types=' + $('#type').val());
-  });
+      var url = 'https://work-wherever.herokuapp.com/google/inj/location=' +
+        location + '&radius=500';
+
+      if ($('#type').val()) { url += '&types=' + $('#type').val(); }
+      if ($('#name').val()) { url += '&name=' + $('#name').val(); }
+      $('#current').text(location).attr('href', url);
+    });
+  };
 
   $('#url').on('blur', function() {
     $('#start').attr('href', 'https://work-wherever.herokuapp.com' +
       $('#url').val());
   });
+
+  $('#type').on('blur', function() {
+    $('#current').text('');
+    getLocation();
+  });
+
+  $('#name').on('blur', function() {
+    $('#current').text('');
+    getLocation();
+  });
+
+  getLocation();
 });
